1f29f1bc76f812148ced7ddf2adf657b8509d103
Polina <polinmailbox@gmail.com>
2019-04-07 23:59:06 +0300

    Added 3D chart to display vectors

--- Clustering/Clustering/AppDelegate.swift
+++ Clustering/Clustering/AppDelegate.swift
@@ -8,10 +8,10 @@
 
 import Cocoa
 
+let chartsLicenseKey = "PTkrDmbx6QeQu+5LtKvfRAWSmeda6qRoQ75JJ+7y9pqvDvbMs6h53FbL5hugE4SVn/PGLxr1g1fb07zH/qylZz4FlYf+T10fQZXNmW6Ae92ZSqc/3e2PQ+FwGxxbY9pfUwafk2QvkiPOR7hcTgqhgPe3juqhfcsNl6pTJwS8LLtd/MnM53Wf8Sl1SFE1iwmUc5XHAYIBMfhq2fl6nzrjbUzKKEZKayyQcEGAjq3iKkkNanC9s/uYGFB+AJhpJEgW7mjazUp354ITAh8HTtJOZFHsxJ268MFEHhbQO2uj97ZjyNc9OanVgi/LvrZSYCcvwEwdW/SacaEpV7ON3lhO7xnEPlsMnBQXSMz0P4FlLC56xCWhIiVKzE3s9X5rn0I/PEuo9o10MtbgisF8oJp+haBsLczgjtrZsVXBvbilzb/2oatJTB47qsn+zFknL3XS8oTRANdjDCfVyxPz71B6KFIyj0J1T5Sm2rjAxVfhDmB+C7E5gMO4jWUm/nRgUPMrkmPJmONgVBmlNGsWUnXet2cW3LtBs1hCgLJ7fZCpG8ZqcMpxvnM7hOatpSMN+ZEOohvKXYPptilRVN+Rr/tqhC3qEFpvnvHmSoBj3L0ZuIKBe42XGJQTG1YqUtGqo1GlTwJSJn1kZlOU1lCAx5mYJ8LSPx5aAhrXYbws2LdnWrk="
+
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
-
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
@@ -20,7 +20,6 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-
 
 }
 
--- Clustering/Clustering/ViewController.swift
+++ Clustering/Clustering/ViewController.swift
@@ -10,30 +10,61 @@ import Cocoa
 
 class ViewController: NSViewController {
     
+    @IBOutlet weak var chartView: NChartView!
     let textHandler = TextHandler()
-    let setTitles = ["Networking", "Medicine", "Sociology"]
+    let setsTitles = ["Networking", "Medicine", "Sociology"]
     var thesaurusArray = [[String]]()
     var vectorsForSetsArray = [[[Int]]]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        chartView.chart.licenseKey = chartsLicenseKey
+        chartView.chart.drawIn3D = true
         
         for i in 0...2 {
-            guard let setOfTexts = textHandler.getSetOfTexts(for: setTitles[i]) else { return }
+            guard let setOfTexts = textHandler.getSetOfTexts(for: setsTitles[i]) else { return }
             thesaurusArray.append(textHandler.createThesaurus(for: setOfTexts))
             printThesaurus(n: i + 1, thesaurus: thesaurusArray[i])
         }
         
         let clasteringHandler = ClasteringHandler(firstThesaurus: thesaurusArray[0], secondThesaurus: thesaurusArray[1], thirdThesaurus: thesaurusArray[2])
         for i in 0...2 {
-            let vectorsForSet = textHandler.getVectorsForSet(set: setTitles[i], clasteringHandler: clasteringHandler)
+            let vectorsForSet = textHandler.getVectorsForSet(set: setsTitles[i], clasteringHandler: clasteringHandler)
             print("\nВектора признаков \(i + 1):")
             for vector in vectorsForSet {
                 print(vector)
             }
             vectorsForSetsArray.append(vectorsForSet)
         }
-        
+    
+        for i in 1...3 {
+            addSeriesToChart(num: i)
+        }
+        self.chartView.chart.updateData()
+    }
+    
+    func createMarker(color: NSColor) -> NChartMarker {
+        let marker = NChartMarker()
+        marker.brush = NChartSolidColorBrush(color: color)
+        marker.shape = NChartMarkerShape.circle
+        marker.size = 8
+        return marker
+    }
+    
+    func addSeriesToChart(num: Int) {
+        let series = NChartBubbleSeries()
+        var markerColor = NSColor.black
+        if num == 1 {
+            markerColor = NSColor(displayP3Red: 140/255, green: 25/255, blue: 88/255, alpha: 1.0)
+        } else if num == 2 {
+            markerColor = NSColor(displayP3Red: 25/255, green: 146/255, blue: 46/255, alpha: 1.0)
+        } else if num == 3 {
+            markerColor = NSColor(displayP3Red: 194/255, green: 0/255, blue: 13/255, alpha: 1.0)
+        }
+        series.marker = createMarker(color: markerColor)
+        series.tag = num
+        series.dataSource = self
+        self.chartView.chart.addSeries(series)
     }
     
     func printThesaurus(n: Int, thesaurus: [String]) {
@@ -53,3 +84,26 @@ class ViewController: NSViewController {
 
 }
 
+//MARK:- NChartSeriesDataSource
+extension ViewController: NChartSeriesDataSource {
+    
+    func seriesDataSourcePoints(for series: NChartSeries!) -> [Any]! {
+        if series.tag >= 1 && series.tag <= 3 {
+            let vectors = vectorsForSetsArray[series.tag - 1]
+            var points = [NChartPoint]()
+            for i in 0...3 {
+                let vector = vectors[i]
+                if let chartPoint = NChartPoint(state: NChartPointState(alignedToXZWithX: vector[0], y: Double(vector[1]), z: vector[2]), for: series) {
+                    points.append(chartPoint)
+                }
+            }
+            return points
+        }
+        return []
+    }
+    
+    func seriesDataSourceName(for series: NChartSeries!) -> String! {
+        return "\(series.tag) set"
+    }
+    
+}
